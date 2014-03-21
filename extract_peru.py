d92af4e64acbd3efdb458a1771706c762dade312
aniversarioperu <aniversarioperu1@gmail.com>
2014-03-21 17:40:41 +0200

    code ready prints out to standard output

---
+++ extract_peru.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+from argparse import RawTextHelpFormatter
+import codecs
+import sys
+
+
+def extract(filename):
+    import os
+    import json
+
+    if os.path.isfile(filename):
+        f = codecs.open(filename, "r", "utf8")
+        data = json.loads(f.read())
+        f.close()
+
+        sismos_peru = []
+        for i in data['features']:
+            if "Peru" in i['properties']['place']:
+                sismos_peru.append(i)
+
+        out = {}
+        out['type'] = 'FeatureCollection'
+        out['features'] = sismos_peru
+        print json.dumps(out)
+
+
+
+def main():
+    description = u"""Este script extrae sismos para Peru usando datos tomados
+    de http://earthquake.usgs.gov/."""
+
+    parser = argparse.ArgumentParser(description=description,formatter_class=RawTextHelpFormatter)
+    parser.add_argument('-f', '--filename', action='store',
+            metavar='file.geojson',
+            help=u'''archivo con datos en formato GeoJSON.''',
+            required=True, dest='filename')
+
+    args = parser.parse_args()
+    filename = args.filename.strip()
+
+    extract(filename)
+
+
+
+if __name__ == "__main__":
+    main()
