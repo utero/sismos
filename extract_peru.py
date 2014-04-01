904cbcc8494fc8d7ad88844c03e58a85d4c643c2
aniversarioperu <aniversarioperu1@gmail.com>
2014-04-01 07:48:26 +0300

    using date limits

--- extract_peru.py
+++ extract_peru.py
@@ -7,10 +7,14 @@ import codecs
 import sys
 
 
-def extract(filename):
+def extract(filename, date_start, date_end):
     import os
     import json
     import time
+    from datetime import datetime
+
+    date_start = datetime.strptime(date_start, "%Y-%m-%d")
+    date_end = datetime.strptime(date_end, "%Y-%m-%d")
 
     if os.path.isfile(filename):
         f = codecs.open(filename, "r", "utf8")
@@ -21,8 +25,10 @@ def extract(filename):
         for i in data['features']:
             if "Peru" in i['properties']['place']:
                 date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(i['properties']['time']/1000))
-                i['properties']['date'] = date
-                sismos_peru.append(i)
+                date_obj = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+                if date_start <= date_obj and date_end >= date_obj:
+                    i['properties']['date'] = date
+                    sismos_peru.append(i)
 
         out = {}
         out['type'] = 'FeatureCollection'
@@ -40,11 +46,22 @@ def main():
             metavar='file.geojson',
             help=u'''archivo con datos en formato GeoJSON.''',
             required=True, dest='filename')
+    parser.add_argument('-s', '--start', action='store',
+            metavar='date start',
+            help=u'''fecha de inicio YYYY-MM-DD''',
+            required=True, dest='date_start')
+    parser.add_argument('-e', '--end', action='store',
+            metavar='date end',
+            help=u'''fecha final YYYY-MM-DD''',
+            required=True, dest='date_end')
 
     args = parser.parse_args()
     filename = args.filename.strip()
 
-    extract(filename)
+    date_start = args.date_start.strip()
+    date_end = args.date_end.strip()
+
+    extract(filename, date_start, date_end)
 
 
 
